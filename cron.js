95acfdd33ea210727bf5f78c51b240dc0f44dfd6
Geoffrey Vedernikoff <yefim323@gmail.com>
2013-09-29 02:16:39 -0400

    Only write if not dup

--- cron.js
+++ cron.js
@@ -1,7 +1,7 @@
 var snapchat = require("./snapchat-client");
 var client = new snapchat.Client();
 var config = require("./config.json");
-var upload = require("./upload");
+var utils = require("./upload");
 var submit = require("./submit");
 var enq = require('./enq');
 
@@ -14,26 +14,28 @@ client.on("sync", function(data) {
 
   data.snaps.forEach(function(snap, i) {
     if (snap.t) {
-      var filename = "snap" + i + ".jpg";
-      var out = require('fs').createWriteStream(filename);
-      client.getBlob(snap.id, out, function (err) { if (err) console.log(err); });
-      out.on("finish", function () {
-        //console.log("download finished for filename: " + filename);
+      utils.dup(snap.id, function() {
+        var filename = "snap" + i + ".jpg";
+        var out = require('fs').createWriteStream(filename);
+        client.getBlob(snap.id, out, function (err) { if (err) console.log(err); });
+        out.on("finish", function () {
+          // console.log("download finished for filename: " + filename);
 
-        upload(filename, snap.id, snap.sn, function(err, dup, url) {
-          if (err) console.log(err);
-          if (!dup) {
-            //queue.push({url: url, title: snap.sn});
-            // IRON.IO SUBMIT GOES HERE
+          utils.upload(filename, snap.id, snap.sn, function(err, dup, url) {
+            if (err) console.log(err);
+            if (!dup) {
+              //queue.push({url: url, title: snap.sn});
+              // IRON.IO SUBMIT GOES HERE
 
-            enq({
-              url: url,
-              title: snap.sn
-            }, function (err) {
-              if (err) console.err(err);
-            });
+              enq({
+                url: url,
+                title: snap.sn
+              }, function (err) {
+                if (err) console.err(err);
+              });
 
-          }
+            }
+          });
         });
       });
     }
--- upload.js
+++ upload.js
@@ -6,44 +6,51 @@ var db = mongo.db('localhost:27017/snapchan', {safe: true}).collection('pics');
 imgur.setClientID('b1cf3448754f15f');
 
 function upload (filename, id, from, callback) {
-  db.findOne({id: id}, {}, function(err, post) {
-    if (!post) {
-      imgur.upload(filename, function (err, imgurRes) {
-        console.log(imgurRes);
+  imgur.upload(filename, function (err, imgurRes) {
+    console.log(imgurRes);
 
-        if (err) {
-          console.dir(err);
-          callback('imgur upload failed');
-        } 
+    if (err) {
+      console.dir(err);
+      callback('imgur upload failed');
+    } 
 
-        else if (!imgurRes.data || !imgurRes.data.link) {
-          // imgur-upload calls my callbakc twice for some reason
-        }
+    else if (!imgurRes.data || !imgurRes.data.link) {
+      // imgur-upload calls my callbakc twice for some reason
+    }
 
-        else {
-          try {
-            db.insert({
-              url: imgurRes.data.link,
-              id: id,
-              from: from
-            }, {}, function (err) {
-              if (err) {
-                console.dir(err);
-                callback('mongo insert failed');
-              } else {
-                //console.log("mongo insert succeeded");
-                callback(null, false, imgurRes.data.link);
-              }
-            }); 
-          } catch (e) { console.dir(e); }
+    else {
+      try {
+        db.insert({
+          url: imgurRes.data.link,
+          id: id,
+          from: from
+        }, {}, function (err) {
+          if (err) {
+            console.dir(err);
+            callback('mongo insert failed');
+          } else {
+            //console.log("mongo insert succeeded");
+            callback(null, false, imgurRes.data.link);
+          }
+        }); 
+      } catch (e) { console.dir(e); }
 
-          fs.unlink(filename, function (err) { 
-            if (err) console.log('Error deleting temporary file: ' + filename); 
-          });
-        }
+      fs.unlink(filename, function (err) { 
+        if (err) console.log('Error deleting temporary file: ' + filename); 
       });
     }
   });
 };
 
-module.exports = upload;
+// only run callback if unique
+function dup(id, callback) {
+  db.findOne({id: id}, {}, function(err, post) {
+    if (err) {
+      console.log(err);
+    } else {
+      if (!post) callback();
+    }
+  });
+};
+
+module.exports = {upload: upload, dup: dup}
