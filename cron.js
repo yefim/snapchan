f86336097db944d83aa11926b9cae8462d808cf3
Geoffrey Vedernikoff <yefim323@gmail.com>
2013-09-28 20:19:06 -0400

    Snapchat upload

---
+++ cron.js
@@ -0,0 +1,24 @@
+var snapchat = require("./snapchat-client");
+var client = new snapchat.Client();
+var config = require("./config.json");
+var upload = require("./upload");
+
+client.login(config.username, config.password);
+
+client.on("sync", function(data) {
+  data.snaps.forEach(function(snap, i) {
+    if (snap.t) {
+      var filename = "snap" + i + ".jpg";
+      var out = require('fs').createWriteStream(filename);
+      client.getBlob(snap.id, out, function(err) { if (err) console.log(err); });
+      out.on("finish", function() {
+        upload(filename, snap.id, snap.sn, function(err, dup) {
+          if (err) console.log(err);
+          if (!dup) {
+            // upload that bitch to reddit
+          }
+        });
+      });
+    }
+  });
+});
--- upload.js
+++ upload.js
@@ -2,10 +2,10 @@ var imgur = require('imgur-upload');
 var mongo = require('mongoskin');
 var fs = require('fs');
 
-var db = mongo.db('localhost:27017/spoonpics', {safe: true}).collection('pics');
+var db = mongo.db('localhost:27017/snapchan', {safe: true}).collection('pics');
 imgur.setClientID('b1cf3448754f15f');
 
-function upload (filename, from, callback) {
+function upload (filename, id, from, callback) {
   imgur.upload(filename, function (err, imgurRes) {
 
     if (err) {
@@ -18,20 +18,27 @@ function upload (filename, from, callback) {
     }
 
     else {
-      try {
-        db.insert({
-          url: imgurRes.data.link,
-          from: from
-        }, {}, function (err) {
-          if (err) {
-            console.dir(err);
-            callback('mongo insert failed');
-          } else {
-            console.log("mongo insert succeeded");
-            callback();
-          }
-        }); 
-      } catch (e) { console.dir(e); }
+      db.findOne({id: id}, function(err, post) {
+        if (!post) {
+          try {
+            db.insert({
+              url: imgurRes.data.link,
+              id: id
+              from: from
+            }, {}, function (err) {
+              if (err) {
+                console.dir(err);
+                callback('mongo insert failed');
+              } else {
+                console.log("mongo insert succeeded");
+                callback(null, false);
+              }
+            }); 
+          } catch (e) { console.dir(e); }
+        } else {
+          callback(null, true);
+        }
+      });
 
       fs.unlink(filename, function (err) { 
         if (err) console.log('Error deleting temporary file: ' + filename); 
